4fab1cd9b6412fa2a46e81602e1534493fcdd850
rolikun <roland.kun98@gmail.com>
2023-09-30 22:28:04 +0200

    refactor(app.ts):refactored entity manager factory

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
-import 'reflect-metadata';
 import { FastifyInstance } from 'fastify';
 import { createFastifyApp } from './framework/fastify/fastify-app';
 import { createApolloServer } from './framework/apollo/apollo-server';
 import { pino } from 'pino';
-import { initConnection } from './framework/typeorm/init-connection';
+import { entityManagerFactory } from './framework/typeorm/entity-manager-factory';
 import { useCasesFactory } from './use-cases/use-cases';
 import { resolversFactory } from './typegraphql/resolvers';
+import { appDataSource } from './framework/typeorm/data-source';
 
 export const createApp = async (): Promise<FastifyInstance> => {
   const logger = pino();
 
-  const connection = initConnection({ logger });
-  const useCases = useCasesFactory({ connection });
+  const entityManager = await entityManagerFactory({
+    logger,
+    dataSource: appDataSource,
+  });
+  const useCases = useCasesFactory({ entityManager });
   const resolvers = resolversFactory({ useCases });
 
   const apolloServer = await createApolloServer({ resolvers });
