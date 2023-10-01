fc56c7973ed53821381a9a3be530878ab25b3bce
rolikun <roland.kun98@gmail.com>
2023-10-01 11:23:26 +0200

    feat(create.input.ts):add class validator

--- src/typegraphql/user/dto/create.input.ts
+++ src/typegraphql/user/dto/create.input.ts
@@ -1,13 +1,18 @@
 import { Field, InputType } from 'type-graphql';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
+  @IsNotEmpty()
   @Field()
   firstName: string;
 
+  @IsNotEmpty()
   @Field()
   lastName: string;
 
+  @IsEmail()
+  @IsNotEmpty()
   @Field()
   email: string;
 }
