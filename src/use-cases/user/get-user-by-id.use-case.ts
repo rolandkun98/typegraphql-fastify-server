43fe6a5f301ad2fd03d692cf5c04bb5c1533f4bf
rolikun <roland.kun98@gmail.com>
2023-10-01 16:02:50 +0200

    feat(user):add created events relation

--- src/use-cases/user/get-user-by-id.use-case.ts
+++ src/use-cases/user/get-user-by-id.use-case.ts
@@ -10,7 +10,7 @@ export const getUserByIdUseCaseFactory =
   async (input) => {
     const user = await entityManager
       .getRepository(User)
-      .findOne({ where: { id: input.id } });
+      .findOne({ where: { id: input.id }, relations: ['createdEvents'] });
     if (!user) {
       throw new Error('The user does not exist!');
     }
